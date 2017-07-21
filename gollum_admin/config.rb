cb2997b6bb2cc3ad87a0686d40283da4d4a9da12
Karl Redman <karl.redman@gmail.com>
2017-07-20 21:15:10 -0500

    update

--- gollum_admin/config.rb
+++ gollum_admin/config.rb
@@ -7,34 +7,37 @@ Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_
 ## * file reference: /var/lib/gems/2.1.0/gems/github-markup-1.6.0/lib/github/markup/command_implementation.rb
 ## * [Adding Pandoc to Gollum - Martin Wolf's weblog [OUTDATED]](https://www.mwolf.net/2014/04/29/adding-pandoc-to-gollum/)
 #ci = ::GitHub::Markup::CommandImplementation.new(
-#      /vimwiki/,
-#      ["Vimwiki"],
-#      "pandoc -f markdown-tex_math_dollars-raw_tex",
-#      #::GitHub::Markup::Markdown.new,
-#      :vimwiki)
+#     /vimwiki/,
+#     ["Vimwiki"],
+#     "pandoc -f markdown-tex_math_dollars-raw_tex",
+#     #::GitHub::Markup::Markdown.new,
+#     :vimwiki)
 
 ## bind your own extension regex (the new set of extensions will also include `.asc` and `.adoc`):
 ## # * file reference: /var/lib/gems/2.1.0/gems/github-markup-1.6.0/lib/github/markups.rb
 #Gollum::Markup.register(:vimwiki,  "Vimwiki")
 #Gollum::Markup.formats[:vimwiki][:regexp] = /vimwiki/
 #GitHub::Markup::markup_impl(:vimwiki, ci)
-##################
+###################
 
-# # bind your own extension regex 
-# # remove the original markdown binding:
-#Gollum::Markup.formats.delete(:markdown)
+# Attempt to replace the primary extension for Markdown
+# remove the original markdown binding:
+Gollum::Markup.formats.delete(:markdown)
 
-# # and define your own (".asc" is the new primary extension):
-# Gollum::Markup.formats[:thing] = {
-#     :name => "Markdown",
-#     :regexp => /thing/
-# }
+# and define your own 
+Gollum::Markup.formats[:thing] = {
+    :name => "Markdown",
+    :regexp => /markdown/
+}
 
 # Gollum::Markup.formats[:markdown][:regexp] = /(?:markdown|md|vimwiki|thing)/
 
-GitHub::Markup.markup(:markdown, :markdown, /md|vimwiki|thing/, ["Markdown"]) do |content|
-	GitHub::Markdown.render(content)
-end
+# #Gollum::Markup.formats.delete(:markdown)
+# GitHub::Markup.markup(:thing, :github_markup, /vimwiki|thing/, ["Thing"]) do |content|
+# 	GitHub::Markdown.render(content)
+# 	#Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(content)
+# 	#Kramdown::Document.new(content).to_html
+# end
 
 
 ############### global options
